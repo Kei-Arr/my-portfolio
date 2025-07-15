1ca7b732baf4fe864b96c6c01e608c6836bb779a
Kei-Arr <rosariokhaylle@gmail.com>
2025-07-15 22:56:04 +0800

    up

--- frontend/src/data/technologies.ts
+++ frontend/src/data/technologies.ts
@@ -8,7 +8,6 @@ export const technologies: Technology[] = [
     {
         name: 'React',
         icon: 'https://img.icons8.com/office/96/react.png',
-        color: 'text-blue-400'
     },
     {
         name: 'HTML5',
@@ -17,27 +16,22 @@ export const technologies: Technology[] = [
     {
         name: 'CSS3',
         icon: 'https://img.icons8.com/pulsar-gradient/96/css3.png',
-        color: 'text-blue-500'
     },
     {
         name: 'Bootstrap',
         icon: 'https://img.icons8.com/office/96/bootstrap.png',
-        color: 'text-purple-500'
     },
     {
         name: 'Tailwind',
         icon: 'https://img.icons8.com/color/96/tailwindcss.png',
-        color: 'text-cyan-400'
     },
     {
         name: 'Express',
         icon: 'https://img.icons8.com/office/96/express-js.png',
-        color: 'text-gray-300'
     },
     {
         name: 'JavaScript',
         icon: 'https://img.icons8.com/color/96/javascript.png',
-        color: 'text-yellow-400'
     },
     {
         name: 'Node.js',
@@ -46,12 +40,10 @@ export const technologies: Technology[] = [
     {
         name: 'MySQL',
         icon: 'https://img.icons8.com/external-those-icons-lineal-color-those-icons/500/external-MySQL-programming-and-development-those-icons-lineal-color-those-icons.png',
-        color: 'text-blue-600'
     },
     {
         name: 'MongoDB',
         icon: 'https://img.icons8.com/color/96/mongo-db.png',
-        color: 'text-green-400'
     },
     {
         name: 'PHP',
@@ -60,27 +52,25 @@ export const technologies: Technology[] = [
     {
         name: 'Laravel',
         icon: 'https://img.icons8.com/nolan/100/laravel.png',
-        color: 'text-red-500'
+      
     },
     {
         name: 'Postman',
         icon: 'https://img.icons8.com/external-tal-revivo-color-tal-revivo/96/external-postman-is-the-only-complete-api-development-environment-logo-color-tal-revivo.png',
-        color: 'text-orange-400'
+       
     },
     {
         name: 'Jira',
         icon: 'https://img.icons8.com/color/96/jira.png',
-        color: 'text-blue-400'
+
     },
     {
         name: 'Bitbucket',
         icon: 'https://img.icons8.com/color/96/bitbucket.png',
-        color: 'text-blue-500'
     },
     {
         name: 'VS Code',
         icon: 'https://img.icons8.com/color/96/visual-studio-code-2019.png',
-        color: 'text-blue-500'
     }
 ];
 
